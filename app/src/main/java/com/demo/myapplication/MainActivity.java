1714f1b34b183e5f84c21826fbf887b89d9d57d3
huanchenchen <1415737202@qq.com>
2017-07-12 19:27:16 +0800

    tiao

--- app/src/main/java/com/demo/myapplication/MainActivity.java
+++ app/src/main/java/com/demo/myapplication/MainActivity.java
@@ -1,13 +1,61 @@
 package com.demo.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.image)
+    ImageView image;
+    @Bind(R.id.linear)
+    LinearLayout linear;
+
+    Timer timer;
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 123:
+                    image.setVisibility(View.GONE);
+                    linear.setVisibility(View.VISIBLE);
+                    timer.cancel();
+                    break;
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        handler.sendEmptyMessage(123);
+                    }
+                }, 3000, 3000);
+            }
+        };
+        thread.start();
     }
+
 }
